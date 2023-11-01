3b59dd0ecd0d10a7905842faef1290bccfbed3ea
Andrey Adriano <dey2010dey@gmail.com>
2023-11-01 10:26:02 -0300

    cria fila de eventos

---
+++ eventqueue.h
@@ -0,0 +1,55 @@
+#ifndef __EVENT_QUEUE__
+#define __EVENT_QUEUE__
+
+#include "fifo.h"
+#include <avr/interrupt.h>
+
+typedef void (*EventFuncPtr)(void*);
+
+struct Event
+{
+    EventFuncPtr func;
+    void * args;
+};
+
+template<int fsize>
+class EventQueue
+{
+private:
+    Fifo<Event*,fsize> events;
+public:
+    EventQueue();
+    ~EventQueue();
+
+    void pushEvent(Event* e)
+    {
+        cli();
+        events.push(e);
+        sei();
+    }
+
+    void run()
+    {
+        Event* e;
+        while(events.size() > 0)
+        {
+            cli();
+            e = events.pop();
+            sei();
+            e->func(e->args);
+        }
+    }
+
+};
+
+template <int fsize>
+inline EventQueue<fsize>::EventQueue()
+{
+}
+
+template <int fsize>
+inline EventQueue<fsize>::~EventQueue()
+{
+}
+
+#endif
